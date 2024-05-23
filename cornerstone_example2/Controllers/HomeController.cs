7072be4c1dfabc9473c495c414932909243b5579
igiahii <giahiiman@gmail.com>
2024-05-23 12:48:35 +0330

    uid of dicom file added

--- cornerstone_example2/Controllers/HomeController.cs
+++ cornerstone_example2/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,7 +20,14 @@ namespace cornerstone_example2.Controllers
         }
         public ActionResult Dicom()
         {
-            string v =  System.Environment.Version.ToString();
+            //string baseUrl = AppDomain.CurrentDomain.BaseDirectory;
+            string baseUrl = string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~"));
+            string path = Path.Combine("Content", "Img", "dicom", "sample1.DCM").Replace("\\", "/");
+            //if (System.IO.File.Exists(path))
+            //{
+            //}
+                ViewBag.BaseUrl = baseUrl;
+                ViewBag.Path = path;
             return View();
         }
     }
